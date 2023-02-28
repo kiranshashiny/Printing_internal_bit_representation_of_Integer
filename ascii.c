f2918f07a619cea6429f0adefc3cae3622c7b7f1
Shashi Kiran <kiranshashiny@gmail.com>
2023-02-28 14:54:38 +0530

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main () {
+
+    int num = 2;
+    int i=0;
+    unsigned int size = sizeof(unsigned int);
+    unsigned int maxPow = 1<<(size*8-1);
+    printf("MAX POW : %u\n",maxPow);
+
+    for ( int count= 3; count < 15; count++ ) {
+            printf ( "i = %d, num = %d\n",i, num );
+	    for(;i<size*8;++i){
+		    // print last bit and shift left.
+		    printf("%u ",num&maxPow ? 1 : 0);
+		    num = num<<1;
+	    }
+	    printf("\n");
+           
+	    i=0;
+	    num = count;
+    }
+    return 1;
+}
